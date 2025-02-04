9fcc474c6ca22f311c7d033919351dabeed65bb5
JulesVerstraeten <jules.verstraeten@student.hogent.be>
2025-02-04 18:44:27 +0100

    Navigation bar to original project

---
+++ youoweme_project/lib/views/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:youoweme_project/views/widgets/bottom_navbar_with_action_button.dart';
+import 'package:youoweme_project/views/widgets/floating_action_button.dart';
+//import 'package:navbar_project/widgets/bottom_navbar_home.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButtonCenterDock(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavbarWithAction(
+        page: "home",
+      ),
+      extendBodyBehindAppBar: false,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              child: Center(
+                child: Text('Home page'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
